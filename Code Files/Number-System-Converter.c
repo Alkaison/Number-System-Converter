bd24c9e2aa924e0abcab87ad8571a173b56b5ff4
Alkaison <505ganeshmourya@gmail.com>
2022-11-13 11:44:00 +0530

    added octal conversion code

--- Code Files/Number-System-Converter.c
+++ Code Files/Number-System-Converter.c
@@ -491,19 +491,96 @@ void decimal_hexadecimal(long int deci)
 }
 
 // Octal Conversion functions 
-void octal_binary(long int octal)
+void octal_binary(long int oct)
 {
+    int rem[50],len=0,decimal=0,i=0,num,ans;
+
+    while(oct!=0)
+    {
+        ans=oct % 10;
+        decimal = decimal + ans * pow(8,i);
+        i++;
+        oct = oct/10;
+    }
+
+    i=0;
+    do
+    {
+        rem[i]=decimal%2;
+        decimal=decimal/2;
+        i++;
+        len++;
+    }
+    while(decimal!=0);
+
+    printf("\nBinary Number : ");
+    for(i=len-1;i>=0;i--)
+    {
+        printf("%d",rem[i]);
+    }
+}
+
+void octal_decimal(long int oct)
+{
+    int decimal=0,i=0,num,ans;
+
+    while(oct!=0)
+    {
+        ans=oct % 10;
+        decimal = decimal + ans * pow(8,i);
+        i++;
+        oct = oct/10;
+    }
+    printf("\nDecimal Number : %d",decimal);
+}
+
+void octal_hexadecimal(long int oct)
+{
+    int rem[50],len=0,decimal=0,i=0,num,ans=0;
+    while(oct!=0)
+    {
+        ans=oct % 10;
+        decimal = decimal + ans * pow(8,i);
+        i++;
+        oct = oct/10;
+    }
     
-}
+    i=0;
+    while(decimal!=0)
+    {
+        rem[i]=decimal%16;
+        decimal=decimal/16;
+        i++;
+        len++;
+    }
 
-void octal_decimal(long int octal)
-{
-
-}
-
-void octal_hexadecimal(long int octal)
-{
-
+    printf("\nHexa-Decimal Number : ");
+    for(i=len-1;i>=0;i--)
+    {
+        switch(rem[i])
+        {
+            case 10:
+                printf("A"); 
+                break;
+            case 11:
+                printf("B"); 
+                break;
+            case 12:
+                printf("C"); 
+                break;
+            case 13:
+                printf("D"); 
+                break;
+            case 14:
+                printf("E"); 
+                break;
+            case 15:
+                printf("F"); 
+                break;
+            default:
+                printf("%d",rem[i]);
+        }
+    }
 }
 
 // Hexadecimal Convesion functions 
